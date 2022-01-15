badc8f67caa6293f5187d84c595322c184c1ad14
Hitesh <hitesh@pop-os.localdomain>
2022-01-15 19:25:38 +0530

    :sparkles: updated acc. to kunal-dsa

--- 08_sorting_algos/05_mergeSort_inplace.cpp
+++ 08_sorting_algos/05_mergeSort_inplace.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 void merge(int *arr, int s, int e) {
-    int mid = (s + e) / 2;
+    int mid = s + (e - s) / 2;
     int i = s;
     int j = mid + 1;
-    int temp[100000];
+    int temp[100000]; // can also use a vector.
     int k = s;
 
+    // similar to merging 2 sorted array/list in another bigger empty array.
     while (i <= mid and j <= e) {
         if (arr[i] < arr[j])
             temp[k++] = arr[i++];
@@ -15,6 +16,7 @@ void merge(int *arr, int s, int e) {
             temp[k++] = arr[j++];
     }
 
+    // if one list/array becomes empty from above loop, just fill the rest of temp with other list.
     while (i <= mid)
         temp[k++] = arr[i++];
 
@@ -35,7 +37,7 @@ void mergeSort(int arr[], int s, int e) {
 
     // do 3 steps:
     // 1. divide the array into 2 equal parts.
-    int mid = (s + e) / 2;
+    int mid = s + (e - s) / 2;
 
     // 2. recursively solve the 2 arrays.
     mergeSort(arr, s, mid);
@@ -55,6 +57,7 @@ int main() {
 
     for (int i = 0; i < n; i++)
         cout << arr[i] << ", ";
+    cout << endl;
 
     return 0;
 }
